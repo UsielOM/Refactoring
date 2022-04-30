333d931739d3b0556155be85476a1c62405eea84
UsielOM <usiel31197@outlook.es>
2022-04-30 15:26:53 -0500

    No tenai nada que ver dos pruebas

---
+++ test/services/FizzbuzzService.test.js
@@ -0,0 +1,13 @@
+const FizzbuzzService = require("../../lib/services/FizzbuzzService")
+
+describe("Tests para FizzbuzzService", () => {
+
+    test("Requerimiento 1: Calcular los explorers en una mision", () => {
+        const explorer1 = { name: "Explorer1", score: 5 }
+        const explorerFizz = FizzbuzzService.applyValidationInExplorer(explorer1)
+        expect(explorerFizz).toBe();
+    })
+
+
+
+})
